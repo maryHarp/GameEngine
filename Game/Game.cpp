58e7a0eb83aaa2c51cd5b3b7140ad19d7ac1f5df
maryHarp <harpergray032@gmail.com>
2026-07-01 09:51:36 -0600

    Add project files.

---
+++ Game/Game.cpp
@@ -0,0 +1,81 @@
+// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include "../Engine/Engine.cpp"
+#include "SDL3/SDL.h"
+
+
+#include <iostream>
+
+//#define PS5
+//
+//#ifdef _DEBUG
+//    #define TEXT "PS5\n"
+//#else
+//#define TEXT "XBOX\n"
+//#endif
+//
+//#define TEXT "hello!\n"
+//#define MAX(a, b) ((a > b) ? a : b)
+
+
+
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == nullptr) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event e;
+    bool quit = false;
+
+    // Define a rectangle
+    SDL_FRect greenSquare{ 270, 190, 200, 200 };
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+
+
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
+        SDL_RenderClear(renderer); // Clear the renderer
+
+        SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255);
+        SDL_RenderPoint(renderer, rand() % 1280, rand() % 1024);
+
+        SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255); // Set render draw color to green
+        SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDebugText(renderer, 30, 30, "Hello World!");
+
+        SDL_RenderPresent(renderer); // Render the screen
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+    //fnEngine();
+}
+
