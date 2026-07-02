d084828933079848c829f3f7d76f22b20fa06b7d
maryHarp <harpergray032@gmail.com>
2026-07-02 10:17:01 -0600

    dir changes and renderer header creation

---
+++ Engine/Renderer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+namespace nu {
+	class Renderer
+	{
+	public:
+		bool Initialize(const char* name, int width, int height);
+		void Shutdown();
+
+		void setColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
+		void Clear();
+		void Present();
+
+		void DrawPoint(float x, float y);
+		void DrawFillRect(float x, float y, float w, float h);
+		void DrawRect(float x, float y, float w, float h);
+
+
+
+	private:
+		SDL_Window* m_window = nullptr;
+		SDL_Renderer* m_renderer = nullptr;
+	};
+}
+
+
+
+
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,13 +1,6 @@
 // Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-
-#include "../Engine/Engine.cpp"
-#include "SDL3/SDL.h"
-
-
-#include <iostream>
-
 //#define PS5
 //
 //#ifdef _DEBUG
@@ -19,35 +12,22 @@
 //#define TEXT "hello!\n"
 //#define MAX(a, b) ((a > b) ? a : b)
 
+#include "Engine.h"
+#include "SDL3/SDL.h"
 
 
+#include <iostream>
 
 int main()
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    nu::Renderer renderer;
 
-    SDL_Window* window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
-    if (window == nullptr) {
-        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-    if (renderer == nullptr) {
-        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+    renderer.Initialize("Game Engine", 1920, 1024);
+    
 
     SDL_Event e;
     bool quit = false;
 
-    // Define a rectangle
-    SDL_FRect greenSquare{ 270, 190, 200, 200 };
-
-    //handle events
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
@@ -55,28 +35,32 @@ int main()
             }
         }
 
+        renderer.setColor(0, 0, 0);
+        renderer.Clear();
 
+        for (int i = 0; i < 1000; i++) {
+            renderer.setColor(rand() % 256, rand() % 256, rand() % 256, rand() % 256);
+            renderer.DrawPoint(rand() % 1280, rand() % 1280);
+             
+        }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
-        SDL_RenderClear(renderer); // Clear the renderer
+        renderer.setColor(rand() % 256, rand() % 256, rand() % 256);
+        renderer.DrawFillRect(40, 40, 50, 50);
+        
 
-        SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255);
-        SDL_RenderPoint(renderer, rand() % 1280, rand() % 1024);
+        //SDL_SetRenderDrawColor(m_renderer, rand() % 256, rand() % 256, rand() % 256, 255); // Set render draw color to green
+        //SDL_RenderFillRect(m_renderer, &greenSquare); // Render the rectangle
 
-        SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255); // Set render draw color to green
-        SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+        //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        //SDL_RenderDebugText(renderer, 30, 30, "Hello World!");
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderDebugText(renderer, 30, 30, "Hello World!");
+        //SDL_RenderPresent(renderer); // Render the screen
+        renderer.Present();
 
-        SDL_RenderPresent(renderer); // Render the screen
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    renderer.Shutdown();
 
     return 0;
-    //fnEngine();
 }
 
