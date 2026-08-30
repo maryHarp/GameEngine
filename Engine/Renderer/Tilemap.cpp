0757cb9b6f698744426d9e8bc26c63edf336258b
maryHarp <harpergray032@gmail.com>
2026-08-29 21:18:31 -0600

    finished tilemap

--- Engine/Renderer/Tilemap.cpp
+++ Engine/Renderer/Tilemap.cpp
@@ -82,6 +82,10 @@ namespace nu {
 	}
 	Vector2 Tilemap::GetTilePosition(const Layer& layer, int tileIndex)
 	{
-		return Vector2();
+		int column = tileIndex % layer.width;
+
+		int row = tileIndex / layer.width;
+
+		return Vector2{ (float)(column * m_tileWidth), (float)(row * m_tileHeight) };
 	}
 }
